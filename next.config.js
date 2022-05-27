eaef6082b11c78b0bc1dd85ef377ddc730c27e94
Joe Kevin Rayan <joekevinrayan1996@gmail.com>
2022-05-27 13:42:13 +1000

    edited next config differently

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  future: { webpack5: true },
+  // future: { webpack5: true },
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false;
+    }
+    return config;
+  },
+};
